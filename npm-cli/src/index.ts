07dd222322ce4da23e295184c58b0e2b65f237f3
Rhazarian <Rhazarian@ya.ru>
2024-11-08 22:51:19 +0100

    update npm-cli

--- npm-cli/src/index.ts
+++ npm-cli/src/index.ts
@@ -1,8 +1,8 @@
-import { execa, type Options as ExecaOptions, type ExecaReturnValue, } from "execa";
+import { execa, type Options as ExecaOptions, type Result } from "execa";
 import { fileURLToPath } from "node:url";
 import { getExePath } from "./get-exe-path.js";
 
-export async function runWarpack(args: string[], execaOptions?: ExecaOptions): Promise<ExecaReturnValue> {
+export async function runWarpack(args: string[], execaOptions?: ExecaOptions): Promise<Result> {
     const exePath = await getExePath();
 
     return execa(fileURLToPath(exePath), args, {
